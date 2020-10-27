aa46c68364a4c16e33c7389b77ec2695834a40e3
Jaeden Hillesheim <Jaedenhillesheim@gmail.com>
2020-10-27 12:05:18 -0700

    Changed wording on room delete

--- BasementRenovator.py
+++ BasementRenovator.py
@@ -2904,8 +2904,8 @@ class RoomSelector(QWidget):
         msgBox = QMessageBox(QMessageBox.Warning,
                 "Delete Room?", "Are you sure you want to delete the selected rooms? This action cannot be undone.",
                 QMessageBox.NoButton, self)
-        msgBox.addButton("Delete", QMessageBox.AcceptRole)
-        msgBox.addButton("Cancel", QMessageBox.RejectRole)
+        msgBox.addButton("Yes", QMessageBox.AcceptRole)
+        msgBox.addButton("No", QMessageBox.RejectRole)
         if msgBox.exec_() == QMessageBox.AcceptRole:
 
             self.list.clearSelection()
